4fea4d93798f70210384a60cda4864a5e4d435e6
Santoshh Tannuu <98578434+santoshtannu@users.noreply.github.com>
2026-04-01 15:28:00 +0530

    Add password generator functionality
    Implemented a password generator that combines letters, symbols, and numbers based on user input.

---
+++ PasswordGenerator.py
@@ -0,0 +1,43 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+start = 0
+password = ""
+#Easy Level
+# password = ""
+# for sym in range(start, nr_symbols):
+#     password += random.choice(symbols)
+#
+# for num in range(start, nr_numbers):
+#     password += random.choice(numbers)
+#
+# for letter in range(start,nr_letters):
+#     password += random.choice(letters)
+#
+# print(password)
+
+# Hard Level
+password_list = []
+for letter in range(start, nr_letters):
+    password_list += random.choice(letters)
+for sym in range(start, nr_symbols):
+    password_list += random.choice(symbols)
+for num in range(start, nr_numbers):
+    password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+for char in password_list:
+    password += char
+
+print(f"Password : {password}")
